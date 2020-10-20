370cfcfa3c7965e05fa471ed8c4c1258011adbc6
Carlosa6 <k5r6ri89yo@gmail.com>
2020-10-20 17:29:10 -0500

    add h5

--- src/components/Display.js
+++ src/components/Display.js
@@ -5,6 +5,7 @@ const Display = ({gameOver,text}) => (
 <p>esddsd4ffdfdfdfdlot</p>
 <p>dsjndnjdsds</p>
 <h2>mensaje de prueba</h2>
+<h5>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis, fugit impedit ipsam dolor delectus sed dolore? At assumenda vel sit voluptatem animi magni rem repellendus, voluptates aliquam fuga aperiam nostrum?</h5>
 </div>
 );
 
